a5c6bfb074f8f6a1966fdbd7a2276ce8b8656f25
smallerhand <mrmu0124@gmail.com>
2018-05-24 16:55:36 +0900

    all

---
+++ Tutorial6/src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int score = 95;
+		if(score >= 90)
+		{
+			System.out.println("A+ 입니다");
+		}
+		else if(score >= 80)
+		{
+			System.out.println("B+ 입니다");
+		}
+		else if(score >= 70)
+		{
+			System.out.println("C+ 입니다");
+		}
+		else
+		{
+			System.out.println("B+ 입니다");
+		}
+
+	}
+
+}
