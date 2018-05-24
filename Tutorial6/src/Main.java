4246b91022bbe85157ba43432870467a45759e42
smallerhand <mrmu0124@gmail.com>
2018-05-24 17:01:51 +0900

    all

--- Tutorial6/src/Main.java
+++ Tutorial6/src/Main.java
@@ -19,7 +19,32 @@ public class Main {
 		{
 			System.out.println("B+ 입니다");
 		}
-
+		String a = "Man";
+		int b = 0;
+		if(a.equals("Man"))
+		{
+			System.out.println("남자입니다");
+		}
+		else
+		{
+			System.out.println("남자가 아닙니다");
+		}
+		if(b == 3)
+		{
+			System.out.println("b는 3입니다");
+		}
+		else
+		{
+			System.out.println("3이 아닙니다");
+		}
+		if(a.equalsIgnoreCase("man")&&b==0)
+		{
+			System.out.println("참입니다");
+		}
+		else
+		{
+			System.out.println("거짓입니다");
+		}
 	}
 
 }
