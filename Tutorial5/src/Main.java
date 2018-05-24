7ddc1bdd858d81eacebdda3daeb19c200a4e0b13
smallerhand <mrmu0124@gmail.com>
2018-05-24 16:50:13 +0900

    all

---
+++ Tutorial5/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "I am a student";
+		if(a.contains("student"))
+		{
+			System.out.print("yes");
+		}
+		else
+		{
+			System.out.print("no");
+		}
+	}
+
+}
