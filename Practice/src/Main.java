61a67bfd58245c8f9ff788bb80d5e6ef671371f7
smallerhand <mrmu0124@gmail.com>
2018-05-24 17:43:43 +0900

    all

---
+++ Practice/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("smallerhand");
+		float a = 10/3;
+		System.out.println(a);
+		System.out.println(395%18);
+		int n = 3, m = 10;
+		for(int i = 0; i <m; i++)
+		{
+			for(int j = 0; j <n; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+
+	}
+
+}
